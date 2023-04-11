0fade23df115b2fbda6b29fc3ba1171ba529a8d8
rohhyungwoo <67363759+rohhyungwoo@users.noreply.github.com>
2023-04-11 13:48:47 +0900

    4-11
    Splash 화면 -> 네비게이션 화면 전환
    1. Coroutine으로 Spalsh 화면 일정 시간 동안 생성
    2. navigation으로 화면 전환 수행

--- app/src/main/java/com/roh/dogdom/MainActivity.kt
+++ app/src/main/java/com/roh/dogdom/MainActivity.kt
@@ -2,8 +2,11 @@ package com.roh.dogdom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.roh.dogdom.base.BaseActivity
 import com.roh.dogdom.databinding.ActivityMainBinding
 
@@ -14,18 +17,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     private fun initNavigation() {
-//        val navController = findNavController(R.id.main_nav_host)
-
-        // BottomNavigation 사용
-//        binding.mainBottomNavigation.setupWithNavController(navController)
-//        binding.mainBottomNavigation.itemIconTintList = null
-
-//        navController.addOnDestinationChangedListener { _, destination, _ ->
-//            if (destination.id == R.id.bottom_nav_1 || destination.id == R.id.bottom_nav_2 || destination.id == R.id.bottom_nav_3 || destination.id == R.id.bottom_nav_4) {
-//                binding.mainBottomNavigation.visibility = View.VISIBLE
-//            } else {
-//                binding.mainBottomNavigation.visibility = View.GONE
-//            }
-//        }
+        // navController 설정
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment
+        val navController : NavController = navHostFragment.findNavController()
     }
+
+    private fun bottomNavigation() {
+        /*// BottomNavigation 사용
+        binding.mainBottomNavigation.setupWithNavController(navController)
+        binding.mainBottomNavigation.itemIconTintList = null
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.bottom_nav_1 || destination.id == R.id.bottom_nav_2 || destination.id == R.id.bottom_nav_3 || destination.id == R.id.bottom_nav_4) {
+                binding.mainBottomNavigation.visibility = View.VISIBLE
+            } else {
+                binding.mainBottomNavigation.visibility = View.GONE
+            }
+        }
+         */
+    }
+
 }
--- app/src/main/java/com/roh/dogdom/views/splash/SplashFragment.kt
+++ app/src/main/java/com/roh/dogdom/views/splash/SplashFragment.kt
@@ -1,21 +1,43 @@
 package com.roh.dogdom.views.splash
 
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.roh.dogdom.R
 import com.roh.dogdom.base.BaseFragment
 import com.roh.dogdom.databinding.FragmentSplashBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class SplashFragment : BaseFragment<FragmentSplashBinding>(R.layout.fragment_splash) {
 
-    private var job = CoroutineScope.launch {
-        // 비동기 작업 처리
-    }
-
+    private lateinit var job : Job
 
     override fun init() {
-        CoroutineScope.launch {
+        showSplashImage()
+    }
 
+    private fun showSplashImage() {
+        job = CoroutineScope(Dispatchers.IO).launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // 비동기 작업 처리
+                delay(1000)
+                // 메인 스레드에서 실행
+                withContext(Dispatchers.Main) {
+                    moveNextScreen()
+                }
+                // 비동기 작업 종료
+                job.cancel()
+            }
         }
     }
+
+    private fun moveNextScreen() {
+        if(isAdded) {
+            findNavController().navigate(
+                SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+            )
+        }
+    }
+
 }
