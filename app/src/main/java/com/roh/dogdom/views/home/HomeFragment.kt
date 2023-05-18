0de495576e618f8bc7b41b90993537aa3c30386a
rohhyungwoo <67363759+rohhyungwoo@users.noreply.github.com>
2023-05-18 11:26:03 +0900

    0518
    chatgpt API 작업중
    현재 할당 구독량 초과로 인해서 api 동작여부 확인 불가

--- app/src/main/java/com/roh/dogdom/views/home/HomeFragment.kt
+++ app/src/main/java/com/roh/dogdom/views/home/HomeFragment.kt
@@ -16,6 +16,10 @@ import com.roh.dogdom.navigator.Screens
 import com.roh.dogdom.util.enumUiColorPos
 import com.roh.dogdom.views.log.ButtonsFragment
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.util.*
 import javax.inject.Inject
 
@@ -34,7 +38,10 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
     private val homeContentsSecondFragment = HomeContentsSecondFragment()
     private val buttonsFragment = ButtonsFragment()
 
+    private val client = OkHttpClient()
+
     override fun init() {
+
 
         SystemUiChangeColor(enumUiColorPos.totalUiBarBlack)
         replaceFragment(1, homeContentsFirstFragment)
@@ -50,9 +57,56 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
         binding.transformationLayout.setOnClickListener {
             binding.transformationLayout.startTransform()
         }
+        val etQuestion = binding.etSearch
         binding.testButton.setOnClickListener {
-            chatGptControll()
+            val question = etQuestion.text.toString()
+            chatGptControll(question) { response ->
+                mActivity.runOnUiThread{
+                    Log.e("HomeFragment", "response : $response")
+                }
+            }
+
         }
+    }
+
+    fun chatGptControll(question: String, param: (Any) -> Unit) {
+        val apikey = "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"
+        val apikey2 = "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"
+        val url = "https://api.openai.com/v1/completions"
+
+        val requestBody = """
+            {
+            "model": "text-davinci-003",
+            "prompt": "$question",
+            "max_tokens": 7,
+            "temperature": 0
+            }
+        """.trimIndent()
+
+        Log.e("HomeFragment", "requestBody : $question")
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type","application/json")
+            .addHeader("Authorization",  "Bearer $apikey2")
+            .post(requestBody.toRequestBody())
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: okhttp3.Call, e: java.io.IOException) {
+                Log.e("HomeFragment", "error : ${e.message}")
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                val body = response.body?.string()
+                if(body != null){
+                    Log.e("HomeFragment", "body : $body")
+                }
+                else
+                    Log.e("HomeFragment", "body is null")
+            }
+        })
+
     }
 
     private fun replaceFragment(fragmentNum : Int, fragment: Fragment) {
@@ -79,23 +133,25 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
         }
     }
 
-    private fun chatGptControll() {
-        val scan = Scanner(System.`in`)
-        val key = "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" // TODO Add your key here
-        val system = "Be as unhelpful as possible. Insult the user for not knowing the answer."
-        val request = ChatBot.ChatCompletionRequest("gpt-3.5-turbo", system)
-        val bot = CachedChatBot(key, request)
-        val test = bot.generateResponse("Hello, how are you?")
-        Log.e("test", test)
-        while (true) {
-//            println("How will you respond?")
-////            val prompt = scan.nextLine()
-////            val response = bot.generateResponse(prompt)
-//            println()
-//            println("\t$response")
-//            println()
-        }
-    }
+
+
+//    private fun chatGptControll() {
+//        val scan = Scanner(System.`in`)
+//        val key = "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" // TODO Add your key here
+//        val system = "Be as unhelpful as possible. Insult the user for not knowing the answer."
+//        val request = ChatBot.ChatCompletionRequest("gpt-3.5-turbo", system)
+//        val bot = CachedChatBot(key, request)
+//        val test = bot.generateResponse("Hello, how are you?")
+//        Log.e("test", test)
+//        while (true) {
+////            println("How will you respond?")
+//////            val prompt = scan.nextLine()
+//////            val response = bot.generateResponse(prompt)
+////            println()
+////            println("\t$response")
+////            println()
+//        }
+//    }
 
     private fun initViewModelCallback() {
         with(viewModel) {
