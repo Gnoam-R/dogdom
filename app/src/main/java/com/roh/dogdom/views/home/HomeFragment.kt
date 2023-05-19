13ace5acc05c0f3b424e44032b4e9064f7c95a6c
rohhyungwoo <67363759+rohhyungwoo@users.noreply.github.com>
2023-05-19 11:38:22 +0900

    05-19
    chatgpt api 적용 테스트 완료
    api 모듈화 및 동작에 대한 처리를 Coroutine & okttp -> retrofit2로 변경 예정

--- app/src/main/java/com/roh/dogdom/views/home/HomeFragment.kt
+++ app/src/main/java/com/roh/dogdom/views/home/HomeFragment.kt
@@ -1,12 +1,13 @@
 package com.roh.dogdom.views.home
 
-import CachedChatBot
-import ChatBot
+
 import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.google.gson.JsonObject
+import com.google.gson.annotations.SerializedName
 import com.roh.dogdom.R
 import com.roh.dogdom.base.BaseFragment
 import com.roh.dogdom.data.home.MainPost
@@ -20,6 +21,8 @@ import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
+import org.json.JSONObject
 import java.util.*
 import javax.inject.Inject
 
@@ -31,6 +34,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
 
     private lateinit var homeAdapter : HomeAdapter
     private lateinit var mainPost : MainPost
+
+    lateinit var messages: MutableList<ChatMessage>
+
 
     private val viewModel by viewModels<HomeViewModel>()
 
@@ -69,26 +75,58 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
         }
     }
 
+    public data class ChatCompletionRequest(
+        val model: String, // recommend: "gpt-3.5-turbo"
+        val messages: MutableList<ChatMessage>,
+        val temperature: Float = 1.0f,
+        @SerializedName("top_p") val topP: Float = 1.0f,
+        val n: Int = 1,
+        val stream: Boolean = false,
+        val stop: String? = null,
+        @SerializedName("max_tokens") val maxTokens: Int? = null, // default is 4096
+        @SerializedName("presence_penalty") val presencePenalty: Float = 0.0f,
+        @SerializedName("frequency_penalty") val frequencyPenalty: Float = 0.0f,
+        @SerializedName("logit_bias") val logitBias: JsonObject? = null,
+        val user: String? = null
+    ) {
+        constructor(model: String, systemContent: String) : this(
+            model,
+            arrayListOf(ChatMessage("system", systemContent))
+        )
+    }
+
     fun chatGptControll(question: String, param: (Any) -> Unit) {
         val apikey = "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"
         val apikey2 = "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"
-        val url = "https://api.openai.com/v1/completions"
+        val godskey = "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"
+//        val url = "https://api.openai.com/v1/chat/completions"
+        val url = "https://api.openai.com/v1/chat/completions"
 
         val requestBody = """
             {
-            "model": "text-davinci-003",
-            "prompt": "$question",
-            "max_tokens": 7,
-            "temperature": 0
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "$question"
+                }
+            ],
+            "temperature": 1,
+            "max_tokens": 512,
+            "top_p": 1,
+            "temperature": 0.5,
+            "frequency_penalty": 0,
+            "presence_penalty": 0 
             }
         """.trimIndent()
+
 
         Log.e("HomeFragment", "requestBody : $question")
 
         val request = Request.Builder()
             .url(url)
             .addHeader("Content-Type","application/json")
-            .addHeader("Authorization",  "Bearer $apikey2")
+            .addHeader("Authorization",  "Bearer $godskey")
             .post(requestBody.toRequestBody())
             .build()
 
@@ -104,9 +142,22 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
                 }
                 else
                     Log.e("HomeFragment", "body is null")
+
+                val jsonObject = JSONObject(body!!)
+                val jsonArray:JSONArray=jsonObject.getJSONArray("choices")
+                Log.e("HomeFragment", "jsonArray : $jsonArray")
+                val textResult = jsonArray.getJSONObject(0).getString("message")
+                Log.e("HomeFragment", "textResult : $textResult")
             }
         })
 
+    }
+
+    public data class ChatMessage(val role: String, val content: String) {
+        constructor(json: JsonObject) : this(
+            json["role"].asString,
+            json["content"].asString
+        )
     }
 
     private fun replaceFragment(fragmentNum : Int, fragment: Fragment) {
@@ -133,25 +184,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
         }
     }
 
-
-
-//    private fun chatGptControll() {
-//        val scan = Scanner(System.`in`)
-//        val key = "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" // TODO Add your key here
-//        val system = "Be as unhelpful as possible. Insult the user for not knowing the answer."
-//        val request = ChatBot.ChatCompletionRequest("gpt-3.5-turbo", system)
-//        val bot = CachedChatBot(key, request)
-//        val test = bot.generateResponse("Hello, how are you?")
-//        Log.e("test", test)
-//        while (true) {
-////            println("How will you respond?")
-//////            val prompt = scan.nextLine()
-//////            val response = bot.generateResponse(prompt)
-////            println()
-////            println("\t$response")
-////            println()
-//        }
-//    }
 
     private fun initViewModelCallback() {
         with(viewModel) {
