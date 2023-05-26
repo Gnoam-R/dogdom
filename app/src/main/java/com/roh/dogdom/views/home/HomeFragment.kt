8d61ba58a97c3394894878279b6b0dc9fd28364d
rohhyungwoo <67363759+rohhyungwoo@users.noreply.github.com>
2023-05-26 18:00:30 +0900

    0526

--- app/src/main/java/com/roh/dogdom/views/home/HomeFragment.kt
+++ app/src/main/java/com/roh/dogdom/views/home/HomeFragment.kt
@@ -32,8 +32,6 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
 
-
-
     // retrofit2 test
     private lateinit var retrofitService: RetrofitService
     private lateinit var retrofit : Retrofit
@@ -46,14 +44,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
 
     lateinit var messages: MutableList<ChatMessage>
 
-
     private val viewModel by viewModels<HomeViewModel>()
 
     private val homeContentsFirstFragment = HomeContentsFirstFragment()
     private val homeContentsSecondFragment = HomeContentsSecondFragment()
     private val buttonsFragment = ButtonsFragment()
-
-    private val client = OkHttpClient()
 
     override fun init() {
         SystemUiChangeColor(enumUiColorPos.totalUiBarBlack)
@@ -74,20 +69,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
             binding.transformationLayout.startTransform()
         }
         val etQuestion = binding.etSearch
-//        binding.testButton.setOnClickListener {
-//            val question = etQuestion.text.toString()
-////
-////            if(question != "")
-////                getSearchList(question)
-//
-//            //okttp3로 하는 방법
-////            chatGptControll(question) { response ->
-////                mActivity.runOnUiThread{
-////                    Log.e("HomeFragment", "response : $response")
-////                }
-////            }
-//
-//        }
     }
 
     private fun getSearchList(question: String) {
@@ -123,71 +104,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
         retrofitService = retrofit.create(RetrofitService::class.java)
     }
 
-    // okttp3 로 하는 방법
-    fun chatGptControll(question: String, param: (Any) -> Unit) {
-//        val apikey = "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"
-//        val apikey2 = "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"
-        val godskey = "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"
-//        val url = "https://api.openai.com/v1/chat/completions"
-        val url = "https://api.openai.com/v1/chat/completions"
-
-        val requestBody = """
-            {
-            "model": "gpt-3.5-turbo",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "$question"
-                }
-            ],
-            "temperature": 1,
-            "max_tokens": 512,
-            "top_p": 1,
-            "frequency_penalty": 0,
-            "presence_penalty": 0 
-            }
-        """.trimIndent()
-
-
-        Log.e("HomeFragment", "requestBody : $question")
-
-        val request = Request.Builder()
-            .url(url)
-            .addHeader("Content-Type","application/json")
-            .addHeader("Authorization",  "Bearer $godskey")
-            .post(requestBody.toRequestBody())
-            .build()
-
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: okhttp3.Call, e: java.io.IOException) {
-//                Log.e("HomeFragment", "error : ${e.message}")
-//            }
-//
-//            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
-//                val body = response.body?.string()
-//                if(body != null){
-//                    Log.e("HomeFragment", "body : $body")
-//                }
-//                else
-//                    Log.e("HomeFragment", "body is null")
-//
-//                val jsonObject = JSONObject(body!!)
-//                val jsonArray:JSONArray=jsonObject.getJSONArray("choices")
-//                Log.e("HomeFragment", "jsonArray : $jsonArray")
-//                val textResult = jsonArray.getJSONObject(0).getString("message")
-//                Log.e("HomeFragment", "textResult : $textResult")
-//            }
-//        })
-
-    }
-
-//    public data class ChatMessage(val role: String, val content: String) {
-//        constructor(json: JsonObject) : this(
-//            json["role"].asString,
-//            json["content"].asString
-//        )
-//    }
-
     private fun replaceFragment(fragmentNum : Int, fragment: Fragment) {
         // 현 Activity 에 연결된 Fragment 관리하는 supportFragmentManager 를 통해 Fragment 전환
         when(fragmentNum){
@@ -212,14 +128,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
         }
     }
 
-
     private fun initViewModelCallback() {
         with(viewModel) {
-//            goMain.observe(viewLifecycleOwner, Observer {
-//                val direction: NavDirections = com.roh.dogdom.views.login.LoginFragmentDirections.actionLoginFragmentToMasterMainFragment()
-//                findNavController().navigate(direction)
-//            })
-//
             goEx.observe(viewLifecycleOwner, Observer {
                 Log.e("MasterMainViewModel", "goEx")
                 goEx()
